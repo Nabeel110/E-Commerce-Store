d5bf774c4646fa73fff23a22a97110e6b6063224
Nabeel Ahmed <nabeelmoolji1@gmail.com>
2020-12-14 21:18:01 +0500

    Updated OrdersRoute

--- Backend/routes/orderRoutes.js
+++ Backend/routes/orderRoutes.js
@@ -2,57 +2,97 @@ const express = require("express");
 const router = express.Router();
 const protect = require("../middleware/authMiddleware");
 const pool = require("../config/helpers");
-const { decodeBase64 } = require("bcryptjs");
 
 // @desc   Create new Order
 // @route  POST /api/orders
 // @access Private
-// router.post("/", protect, (req, res) => {
-//   const {
-//     orderItems,
-//     shippingAddress,
-//     paymentMethod,
-//     itemsprice,
-//     taxPrice,
-//     shippingPprice,
-//     totalPrice,
-//   } = req.body;
+router.post("/", protect, (req, res) => {
+  const {
+    orderItems,
+    paymentMethod,
+    itemsprice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+    address,
+    city,
+    postalCode,
+    country,
+  } = req.body;
 
-//   const userData = {
-//     user_id: req.id,
-//   };
+  const userData = {
+    user_id: req.id,
+  };
 
-//   // const orderItems ={
+  let insertOrderQuery = `INSERT INTO ORDERS SET ?`;
+  let paymentQuery = "INSERT INTO PAYMENT SET ?";
+  let shippingQuery = "INSERT INTO SHIPPING SET ?";
+  let orderDetailsQuery = "INSERT INTO ORDERS_DETAILS SET ?";
 
-//   // }
-//   console.log(req.id);
-//   res.status(200).json({
-//     message: "No orders yet",
-//   });
+  if (orderItems && orderItems.length == 0) {
+    res.status(400).json({
+      message: "No order Items",
+    });
+  } else {
+    pool.query(insertOrderQuery, userData, (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        const paymentFields = {
+          paymentMethod,
+          itemsprice,
+          taxPrice,
+          shippingPrice,
+          totalPrice,
+          order_id: result.insertId,
+        };
+        pool.query(paymentQuery, paymentFields, (err, result1) => {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log(result1);
+          }
+        });
 
-//   if (orderItems && orderItems.length === 0) {
-//     res.status(400).json({
-//       message: "No order Items",
-//     });
-//   } else {
-//     let insertOrderQuery = `INSERT INTO ORDERS SET ?`;
-//     let insertOrderItems = `INSERT INTO ORDER_DETAILS SET ?`;
+        const shippingFields = {
+          address,
+          city,
+          postalCode,
+          country,
+          order_id: result.insertId,
+        };
+        pool.query(shippingQuery, shippingFields, (err, result2) => {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log(result2);
+          }
+        });
 
-//     pool.query(insertOrderQuery, userData, (err, result) => {
-//       if (err) {
-//         console.log(err);
-//       } else {
-//         pool.query(insertOrderItems, userData, (err, result) => {
-//             if (err) {
-//               console.log(err);
-//             } else {
-
-//             }
-//       }
-//     );
-//   }
-// })
-// });
+        orderItems.forEach(async (order) => {
+          pool.query(
+            orderDetailsQuery,
+            {
+              product_id: order.product,
+              order_id: result.insertId,
+              quantity: order.qty,
+            },
+            (err, result3) => {
+              if (err) {
+                console.log(err);
+              } else {
+                console.log(result3);
+              }
+            }
+          );
+        });
+      }
+    });
+    res.status(201).json({
+      id: req.id,
+    });
+  }
+});
 
 // db.end();
 module.exports = router;
